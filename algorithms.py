a2b3b50a5ffc7103ee15e313a0516868efee251b
Nilotpal Rajbongshi <nilotpalrajbongshi@Nilotpals-MacBook-Air.local>
2025-05-10 15:20:43 -0700

    added q learning for alogrithms file

--- algorithms.py
+++ algorithms.py
@@ -12,30 +12,46 @@ class RandomAgent:
     def choose_action(self, state):
         return np.random.randint(self.action_size)
 
+
+def get_state_key(state):
+    # Convert state to a hashable key
+    if isinstance(state, np.ndarray):
+        return tuple(state.flatten())
+    elif isinstance(state, (list, tuple)):
+        return tuple(state)
+    else:
+        return state
+
+
 class QLearningAgent:
-    def __init__(self, action_size, learning_rate=0.1, discount_factor=0.99, epsilon=0.1):
+    def __init__(self, action_size, learning_rate=0.1, discount_factor=0.95, epsilon=1.0, epsilon_decay=0.995, epsilon_min=0.01):
         self.action_size = action_size
-        self.learning_rate = learning_rate
-        self.discount_factor = discount_factor
+        self.q_table = {}  # Dictionary to store Q-values
+        self.lr = learning_rate
+        self.gamma = discount_factor
         self.epsilon = epsilon
-        self.q_table = {}
-
-    def get_q_value(self, state, action):
-        return self.q_table.get((tuple(state), action), 0.0)
-
-    def set_q_value(self, state, action, value):
-        self.q_table[(tuple(state), action)] = value
+        self.epsilon_decay = epsilon_decay
+        self.epsilon_min = epsilon_min
 
     def choose_action(self, state):
-        if random.uniform(0, 1) < self.epsilon:
+        state_key = get_state_key(state)
+        if random.random() < self.epsilon:
             return random.randint(0, self.action_size - 1)
-        q_values = [self.get_q_value(state, a) for a in range(self.action_size)]
-        max_q = max(q_values)
-        best_actions = [a for a, q in enumerate(q_values) if q == max_q]
-        return random.choice(best_actions)
+        else:
+            self.q_table.setdefault(state_key, np.zeros(self.action_size))
+            return int(np.argmax(self.q_table[state_key]))
 
     def update(self, state, action, reward, next_state, done):
-        future_q = 0 if done else max(self.get_q_value(next_state, a) for a in range(self.action_size))
-        current_q = self.get_q_value(state, action)
-        new_q = current_q + self.learning_rate * (reward + self.discount_factor * future_q - current_q)
-        self.set_q_value(state, action, new_q)
+        state_key = get_state_key(state)
+        next_state_key = get_state_key(next_state)
+
+        self.q_table.setdefault(state_key, np.zeros(self.action_size))
+        self.q_table.setdefault(next_state_key, np.zeros(self.action_size))
+
+        best_next_action = np.max(self.q_table[next_state_key])
+        td_target = reward + self.gamma * best_next_action * (0 if done else 1)
+        td_error = td_target - self.q_table[state_key][action]
+        self.q_table[state_key][action] += self.lr * td_error
+
+        if done:
+            self.epsilon = max(self.epsilon_min, self.epsilon * self.epsilon_decay)
